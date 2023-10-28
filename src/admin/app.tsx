cd775bcf46224bff0906a3eff42bafb545734b4a
Daniel Mendes <daniel.mendes@metropoles.com>
2023-10-28 20:46:17 -0300

    Customizando Logo e Favicon

---
+++ src/admin/app.tsx
@@ -0,0 +1,19 @@
+import Icon from './extensions/icon.png'
+import Logo from './extensions/logo.svg'
+
+export default {
+  config: {
+    locales: [],
+    auth: {
+      logo: Logo,
+    },
+    head: {
+      favicon: Icon,
+    },
+    menu: {
+      logo: Icon,
+    },
+  },
+  bootstrap() {
+  },
+};
